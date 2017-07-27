2d8721f83929b486b0952fff0b8ed1d91aaadf6f
Will <baldesw08@gmail.com>
2017-07-27 16:41:29 -0400

    Added a test

---
+++ app/src/test/java/posidenpalace/com/travis/UnitTests.java
@@ -0,0 +1,16 @@
+package posidenpalace.com.travis;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created by Android on 7/27/2017.
+ */
+
+public class UnitTests {
+    @Test
+    public void testMath() throws Exception{
+        assertEquals("Hello World","Hello"+"World");
+    }
+}
